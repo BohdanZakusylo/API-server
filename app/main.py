2c0e7483d3acc20d338f792517caf1939b7141af
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2023-12-11 10:51:28 +0100

    created the folder for token

--- app/main.py
+++ app/main.py
@@ -1,25 +1,31 @@
 import os
 import pyodbc
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 # main connection variables
 
 app = FastAPI()
 
-load_dotenv(".env")
+# load_dotenv(".env")
 
-SERVER = os.getenv("SERVER")
-DATABASE = os.getenv("DATABASE")
-USERNAME = os.getenv("USER")
-PASSWORD = os.getenv("PASSWORD")
+# SERVER = os.getenv("SERVER")
+# DATABASE = os.getenv("DATABASE")
+# USERNAME = os.getenv("USER")
+# PASSWORD = os.getenv("PASSWORD")
+PUBLIC_KEY = os.getenv("PUBLiC_KEY")
 
 
-connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD};TrustServerCertificate=yes'
+# connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD};TrustServerCertificate=yes'
 
-conn = pyodbc.connect(connectionString)
+# conn = pyodbc.connect(connectionString)
 
 
 @app.get("/")
-async def root():
+async def main_page():
     return {"message": "Hello World"}
+
+
+@app.post("/login")
+async def login(name: str = Query(...), password: str = Query(...), email: str = Query(...)):
+

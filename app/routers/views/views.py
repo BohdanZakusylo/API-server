41be35abda77af5034346f25b0ebed663f4e00b6
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2024-02-24 22:17:55 +0100

    added views filmgenre and prefattr

---
+++ app/routers/views/views.py
@@ -0,0 +1,365 @@
+import app.common as common
+import app.connection as connection
+
+conn, cursor = connection.conn, connection.cursor
+
+attributes_router = common.APIRouter()
+
+oauth2_scheme = common.OAuth2PasswordBearer(tokenUrl="token")
+
+correct_data = common.Correct_Data()
+views_router = common.APIRouter()
+
+@views_router.get("/episode-dubbing")
+async def get_view_episode_dubbing(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewEpisodeDubbing];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.IntegrityError:
+        raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "episode-dubbing-view")
+
+@views_router.get("/episode-subtitle")
+async def get_view_episode_subtitle(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewEpisodeSubtitle];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        print(error_message)
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "episode-subtitle-view")
+
+@views_router.get("/series-episodes")
+async def get_view_series_episodes(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewEpisodesPerSeries];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        print(error_message)
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "series-episodes-view")
+
+@views_router.get("/film-attribute")
+async def get_view_film_attribute(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewFilmAttribute];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        print(error_message)
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "film-attribute-view")
+
+@views_router.get("/film-dubbing")
+async def get_view_film_dubbing(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewFilmDubbing];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        print(error_message)
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "film-dubbing-view")
+
+@views_router.get("/film-quality")
+async def get_view_film_quality(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewFilmQuality];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        print(error_message)
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "film-quality-view")
+
+@views_router.get("/film-subtitle")
+async def get_view_film_quality(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewFilmSubtitle];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        print(error_message)
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "film-subtitle-view")
+
+@views_router.get("/profile-watchlist-film")
+async def get_view_profile_watchlist_film(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewProfileWatchlistFilm];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        print(error_message)
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "profile-watchlist-film-view")
+
+@views_router.get("/profile-watchlist-series")
+async def get_view_profile_watchlist_series(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewProfileWatchlistSeries];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        print(error_message)
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "profile-watchlist-series-view")
+
+# @views_router.get("/profile-watchlist-all")
+
+@views_router.get("/profile-preferred-attribute")
+async def get_view_profile_preferred_attribute(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewProfilePreferredAttribute];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "profile_preferred_attribute-view")
+
+@views_router.get("/series-genre")
+async def get_view_series_genre(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewSeriesGenre];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "series-genre-view")
+
+@views_router.get("/user-information")
+async def get_view_user_information(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewUserInformation];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "user-information-view")
+
+#dbms error
+@views_router.get("/user-profile")
+async def get_view_user_profile(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewUserProfile];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "user-profile-view")
+
+@views_router.get("/episode-view")
+async def get_view_episode_view(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewEpisodeView];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "view-episode-view")
+
+@views_router.get("/film-view")
+async def get_view_film_view(accept: str = common.Header(default="application/json"), token: str = common.Depends(oauth2_scheme)):
+    common.decode_token(token)
+
+    try:
+        cursor.execute(f"EXEC [SelectViewFilmView];")
+        rows = cursor.fetchall()
+        result_list = []
+
+        for row in rows:
+            user_dict = {}
+            for idx, column in enumerate(cursor.description):
+                user_dict[column[0]] = str(row[idx])
+            result_list.append(user_dict)
+        response = {"status": "200 OK", "data": result_list}
+
+    except common.pyodbc.ProgrammingError as programming_error:
+        error_code, error_message = programming_error.args
+        if error_code == '42000' and 'The EXECUTE permission was denied on the object' in error_message:
+            raise common.HTTPException(status_code=403, detail="Permission denied")
+
+    return correct_data.return_correct_format(response, correct_data.validate_data_type(accept) , "view-film-view")
